5b6a1336eb65bbf79ad9230d224c84eaae390a42
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-10-17 13:42:57 -0700

    trying to implement google sign in

--- frontend/src/views/Landing.js
+++ frontend/src/views/Landing.js
@@ -4,6 +4,18 @@ import Layout from '../components/Layout/Layout';
 import LandingButton from '../components/Landing/LandingButton';
 import TestDataVisualization from '../views/TestDataVisualization';
 import { connect } from 'react-redux';
+import { Google } from 'expo';
+
+const googleSignIn = async () => {
+  const { type, accessToken, user } = await Google.logInAsync({
+    androidClientId: `449923889220-pa3veecaq72o4tiairfrputrj7f0dp2n.apps.googleusercontent.com`
+  });
+
+  if (type === 'success') {
+    /* `accessToken` is now valid and can be used to get data from the Google API with HTTP requests */
+    console.log(user);
+  }
+};
 
 const Landing = props => {
   const { isLoggedIn } = props;
@@ -14,10 +26,7 @@ const Landing = props => {
         <TestDataVisualization />
       ) : (
         <View>
-          <LandingButton
-            title='Sign Up'
-            onPress={() => props.navigation.navigate('SignUp')}
-          />
+          <LandingButton title='Sign Up' onPress={() => googleSignIn()} />
           <LandingButton
             title='Login'
             onPress={() => props.navigation.navigate('Login')}
