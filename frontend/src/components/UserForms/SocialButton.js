71053269e71d66326afc497209068456d64177d6
VulpineReynard <milare8@gmail.com>
2019-10-17 17:29:21 -0400

    google sign-in implemented, will add functions to update state with user info from google

--- frontend/src/components/UserForms/SocialButton.js
+++ frontend/src/components/UserForms/SocialButton.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { IconButton } from 'react-native-paper';
 
 const SocialButton = props => {
-  return <IconButton icon={props.icon} size={36} />;
+  console.log(props);
+  return <IconButton onPress={() => props.googleSignIn()} icon={props.icon} size={36} />;
 };
 
 export default SocialButton;
