7c345f6e1335dda59d6ec0d4c81c65800e873ebf
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-10-23 10:49:20 -0700

    MyRecipes page is now displaying data with redux. Added styling to match theme

--- frontend/src/views/MyRecipes.js
+++ frontend/src/views/MyRecipes.js
@@ -1,23 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Layout from '../components/Layout/Layout';
+import { connect } from 'react-redux';
+import { getUserRecipes } from '../store/actions/index.js';
+import Navbar from '../components/Layout/Navbar';
 
-const MyRecipes = () => {
-  const [recipes, setRecipes] = useState([
-    {
-      id: 1,
-      title: 'Nitro Cold Brew'
-    },
-    {
-      id: 2,
-      title: 'Vanilla Latte'
-    },
-    {
-      id: 3,
-      title: 'Vanilla Cappuccino'
-    }
-  ]);
+const MyRecipes = props => {
+  useEffect(() => {
+    props.getUserRecipes();
+  }, []);
+  // const [recipes, setRecipes] = useState([
+  //   {
+  //     id: 1,
+  //     title: 'Nitro Cold Brew'
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Vanilla Latte'
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Vanilla Cappuccino'
+  //   }
+  // ]);
   return (
     <View style={styles.pageContainer}>
       <Text style={styles.recipesHeader}>My Recipes</Text>
@@ -25,21 +31,22 @@ const MyRecipes = () => {
       <View style={styles.recipesContainer}>
         <FlatList
           keyExtractor={item => item.id.toString()}
-          data={recipes}
+          data={props.userRecipes}
           renderItem={({ item, index }) => (
             <View style={styles.recipeContainer}>
-              <Text>{item.title}</Text>
+              <Text style={styles.recipeTitle}>{item.title}</Text>
               <View style={styles.recipeInfoContainer}>
                 <View style={styles.recipeInfo}>
-                  <Text>85</Text>
+                  <Text>Pour Over</Text>
+                </View>
+                <View style={styles.recipeInfo}>
+                  <Text>{item.water_temp}</Text>
                   <MaterialCommunityIcons
                     name={'temperature-fahrenheit'}
                     size={16}
                     color={'black'}
                   />
                 </View>
-                <Text>Coarseness: fine</Text>
-                <Text>hello</Text>
               </View>
             </View>
           )}
@@ -54,32 +61,46 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     paddingVertical: 48,
-    paddingHorizontal: 24
+    paddingHorizontal: 24,
+    backgroundColor: '#ece6cf'
   },
   recipesHeader: {
     fontSize: 24
   },
   recipesContainer: {
-    marginTop: 32
+    marginTop: 24
   },
   recipeContainer: {
     width: '100%',
-    backgroundColor: '#ccc',
+    backgroundColor: 'white',
     marginVertical: 8,
     padding: 16,
     justifyContent: 'center',
     borderRadius: 5
   },
+  recipeTitle: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
   recipeInfoContainer: {
     width: '100%',
-    marginTop: 16,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    marginTop: 12
   },
   recipeInfo: {
     flexDirection: 'row'
-  },
-  recipeIcon: {}
+  }
 });
 
-export default MyRecipes;
+const mapStateToProps = state => {
+  return {
+    userRecipes: state.userRecipes.userRecipes,
+    isLoading: state.userRecipes.isLoading
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getUserRecipes }
+)(MyRecipes);
