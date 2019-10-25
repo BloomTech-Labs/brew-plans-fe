dac5945424a4517ed9567392579ac5a583bdb936
Eli Citron <ebcitron@gmail.com>
2019-10-25 16:16:22 -0500

    Working on form component

--- frontend/src/views/MyRecipes.js
+++ frontend/src/views/MyRecipes.js
@@ -70,6 +70,7 @@ const MyRecipes = props => {
                   )
                 }
                 delete={() => props.deleteUserRecipe(recipe.id)}
+                
               />
             ))}
           </ScrollView>
