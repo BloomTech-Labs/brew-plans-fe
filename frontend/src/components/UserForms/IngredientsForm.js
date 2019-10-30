98117d8b73fd996d37e2b8d588ef3f71b35a54cc
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-10-30 08:47:08 -0700

    trying to fix bug in IngredientsForm

--- frontend/src/components/UserForms/IngredientsForm.js
+++ frontend/src/components/UserForms/IngredientsForm.js
@@ -1,45 +1,49 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
-import {OurButton as IngredientButton} from './SubmitButton';
+import { OurButton as IngredientButton } from './SubmitButton';
 import { withTheme, TextInput } from 'react-native-paper';
 import { connect } from 'react-redux';
+import { handleNewRecipeInput } from '../../store/actions/index';
 
 const IngredientsForm = props => {
-    const { ingredients } = props;
-    
-    // {ingredients.length > 0 ? (
-       // ingredients.map((ingredient, index) => {
-        return (
-        <View style={styles.forView}>
-            <TextInput
-            style={styles.formInput}
-            onChangeText = {(value) => handleNewRecipeInput('Ingredients', value)}
-            // onBlur={props.handleBlur('Ingredients')}
-            value={ingredient} // cha[index]nge this!
-            label='Ingredients'
-            mode='outlined'
-            placeholder='What goes into the recipe?'
-            /> 
-            <IngredientButton title="Delete" onPress={ingredients -= value}/>
-        </View> )
-    // )})
-    // ) : (
-    //     <View>
-    //         <TextInput
-    //         style={styles.formInput}
-    //         onChangeText = {() => {}}
-    //         // onBlur={props.handleBlur('Additional Ingredient?')}
-    //         value={ingredients} // change this!
-    //         label='Additional Ingredient?'
-    //         mode='outlined'
-    //         placeholder='Anything Else you would like to Add?' />
-    //     </View>
-    // )}
-//
+  const { ingredients } = props;
 
-        }
+  // {ingredients.length > 0 ? (
+  // ingredients.map((ingredient, index) => {
+  return (
+    <View style={styles.formView}>
+      <TextInput
+        style={styles.formInput}
+        onChangeText={value => handleNewRecipeInput('Ingredients', value)}
+        // onBlur={props.handleBlur('Ingredients')}
+        // value={ingredient} // cha[index]nge this!
+        label='Ingredients'
+        mode='outlined'
+        placeholder='What goes into the recipe?'
+      />
+      <IngredientButton
+        title='Delete'
+        //  onPress={(ingredients -= value)}
+      />
+    </View>
+  );
+  // )})
+  // ) : (
+  //     <View>
+  //         <TextInput
+  //         style={styles.formInput}
+  //         onChangeText = {() => {}}
+  //         // onBlur={props.handleBlur('Additional Ingredient?')}
+  //         value={ingredients} // change this!
+  //         label='Additional Ingredient?'
+  //         mode='outlined'
+  //         placeholder='Anything Else you would like to Add?' />
+  //     </View>
+  // )}
+  //
+};
 const styles = {
-      formView: {
+  formView: {
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%'
@@ -55,18 +59,16 @@ const styles = {
     marginBottom: 16,
     width: '90%',
     fontSize: 3
-  },
-}
+  }
+};
 
 const mapStateToProps = state => {
-    return {
-        ingredients: state.userRecipes.newRecipe.ingredients
-    };
+  return {
+    ingredients: state.userRecipes.newRecipe.ingredients
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    {
-
-    }
+  mapStateToProps,
+  { handleNewRecipeInput }
 )(IngredientsForm);
