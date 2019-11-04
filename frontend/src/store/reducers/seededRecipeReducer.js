816e416cf44ac5b032788e850d57cd572c6d8d5d
Eli Citron <ebcitron@gmail.com>
2019-11-04 11:56:36 -0600

    BrewPlans Seeded Recipes Rendering

--- frontend/src/store/reducers/seededRecipeReducer.js
+++ frontend/src/store/reducers/seededRecipeReducer.js
@@ -20,7 +20,7 @@ const seededRecipeReducer = (state = initialState, action) => {
 
     case GET_SEEDED_RECIPES_SUCCESS: 
     const recipes = action.payload;
-    recipes.instructions.split("////")
+    //recipes.instructions.split("////")
       return {
         ...state,
         seededRecipes: recipes,
