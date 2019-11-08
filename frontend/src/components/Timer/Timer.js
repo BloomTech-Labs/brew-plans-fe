569edaf66bb5cd96dd1a1940a43d48b0814e1015
Preston <pres.10.nap@gmail.com>
2019-11-08 10:53:31 -0500

    timer package added

--- frontend/src/components/Timer/Timer.js
+++ frontend/src/components/Timer/Timer.js
@@ -1,70 +1,22 @@
 import React from 'react';
-import StartButton from '../Timer/StartButton';
-// import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-// import { connect } from 'react-redux';
 
 
-const Timer = props => {
-    const { time } = props;
-     this.state = {
-      s: '00',   // seconds state 
-      m: ''  // minutes state
+import CountDown from 'react-native-countdown-component';
+
+ const Timer = props => {
+
+// render() {
+    return (
+      <CountDown
+        until={60 * 10 + 30}
+        size={30}
+        onFinish={() => alert('Finished')}
+        digitStyle={{backgroundColor: '#FFF'}}
+        digitTxtStyle={{color: '#1CC625'}}
+        timeToShow={['M', 'S']}
+        timeLabels={{m: 'MM', s: 'SS'}}
+      />
+    )
 }
 
-// //VVVV working on changing this from class components VVVV
-
-// this.remainingSeconds; 
-// this.intervalHandle;
-
-// this.startCountDown = this.startCountDown.bind(this);
-// this.tick = this.tick.bind(this);
-// }
-
-// this.handleChange = this.handleChange.bind(this);
-
-// const handleChange (event) {
-//     this.setState({
-//       m: event.target.value
-//     })
-//  }
-
-// tick() {
-//     var min = Math.floor(this.remainingSeconds / 60);
-//     var sec = this.remainingSeconds - (min * 60);
-//     this.setState({
-//       m: min,
-//       s: sec
-//     })
-//     if (sec < 10) {
-//       this.setState({
-//         s: "0" + this.state.s,
-//       })
-//     }
-//     if (min < 10) {
-//     this.setState({
-//       value: "0" + min,
-//      })
-//     }
-//     if (min === 0 & sec === 0) {
-//     clearInterval(this.intervalHandle);
-//     }
-//     this.remainingSeconds--
-//     }
-//     startCountDown() {
-//     this.intervalHandle = setInterval(this.tick, 1000);
-//     let time = this.state.m;
-//     this.remainingSeconds = time * 60;
-//     }
-//      }
-
-
- return (
-    <div>
-     <Timer m={this.state.m} handleChange={this.handleChange}/>
-     <Timer m={this.state.m} s={this.state.s}/>
-    <StartButton/>
-   </div>
-  );
-}
-
- export default Timer();
+export default Timer;
