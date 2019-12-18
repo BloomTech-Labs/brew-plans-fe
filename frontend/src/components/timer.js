8aa02fb4eeb5d7d23cdb6803f335e925de42b8ca
tlaudahl <travis.laudahl@gmail.com>
2019-12-18 16:38:20 -0600

    Timer no longer goes below zero if 2 timers are running at the same time

--- frontend/src/components/timer.js
+++ frontend/src/components/timer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, TouchableOpacity, Text, Button } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import {Audio} from "expo-av";
@@ -9,12 +9,17 @@ import styles from "../styling/TimerStyling";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
+    console.log('Timer Props', props);
     this.state = {
       min: (Math.floor((props.stepLength) / 60))
       || 0,
       sec: (props.stepLength % 60).toString() || 0,
       startDisabled: false,
     };
+    this.timer = null;
+    this.startTimer = this.startTimer.bind(this);
+    this.countdown = this.countdown.bind(this);
+    // this.pause = this.pause.bind(this);
   }
 
   timer = {};
@@ -22,42 +27,68 @@ class Timer extends React.Component {
 
   async componentDidMount() {
     try {
-       await this.soundObject.loadAsync(require("../../assets/coffee-song.mp3"))
+        await this.soundObject.loadAsync(require("../../assets/coffee-song.mp3"))
     } catch(error) {
       console.log("error", error)
     }
   }
 
-  countdown() {
-    // console.log("pressed button in function ");
-    timer = setInterval(async() => {
-      // console.log("min", this.state.min, "sec", this.state.sec);
-      var seconds = Number(this.state.sec) - 1,
-        minutes = Number(this.state.min)
+  // countdown() {
+  //   console.log(this.state);
+  //   // console.log("pressed button in function ");
+  //   timer = setInterval(() => {
+  //     console.log("min", this.state.min, "sec", this.state.sec);
+  //     let seconds = Number(this.state.sec) - 1,
+  //       minutes = Number(this.state.min)
 
-      if (this.state.sec == 0) {
-        minutes = Number(this.state.min) - 1;
-        seconds = 59;
-      }
-      this.setState({ min: minutes.toString() });
-      this.setState({ sec: seconds.toString() });
-      this.setState({ startDisabled: true });
-      if (seconds === 0 && minutes === 0) {
-        clearInterval(timer);
-        await this.soundObject.playAsync()
-      }
-    }, 1000);
+  //     if (seconds == 0) {
+  //       // console.log('seconds == 0')
+  //       // minutes = Number(this.state.min);
+  //       // seconds = 59;
+  //       clearInterval(timer)
+  //     }
+  //     this.setState({ min: minutes.toString() });
+  //     this.setState({ sec: seconds.toString() });
+  //     this.setState({ startDisabled: true });
+  //     if (seconds === 0 && minutes === 0) {
+  //       console.log('timer let', timer);
+  //       clearInterval(timer);
+  //       // await this.soundObject.playAsync()
+  //     }
+  //   }, 1000);
+  // }
+
+  countdown() {
+    if(this.state.sec > 0) {
+      this.setState({ sec: (this.state.sec - 1).toString() })
+    } else if(this.state.sec == 0 && this.state.min == 0) {
+      clearInterval(this.timer);
+    } else if(this.state.sec == 0 && this.state.min > 0) {
+      this.setState({ sec: 59, min: this.state.min - 1 })
+    }
+  }
+
+  startTimer() {
+    const { startDisabled } = this.state;
+    if(!startDisabled) {
+      this.timer = setInterval(this.countdown, 1000);
+      this.setState({ startDisabled: true })
+    } else {
+      clearInterval(this.timer)
+      this.setState({ startDisabled: false })
+    }
   }
 
   reset() {
-    clearInterval(timer);
+    clearInterval(this.timer);
     this.setState({ min: Math.floor(this.props.stepLength / 60) });
     this.setState({ sec: (this.props.stepLength % 60).toString() });
     this.setState({ startDisabled: false });
   }
 
   pause() {
-    clearInterval(timer);
+    console.log('pausing');
+    clearInterval(this.timer);
     this.setState({ startDisabled: false });
   }
 
@@ -78,6 +109,7 @@ class Timer extends React.Component {
           <TouchableOpacity>
             {this.state.startDisabled ? (
               <Ionicons
+                disabled
                 name="md-pause"
                 size={32}
                 color="#720A13"
@@ -93,7 +125,7 @@ class Timer extends React.Component {
                 color="#720A13"
                 style={styles.icons}
                 onPress={() => {
-                  this.countdown();
+                  this.startTimer();
                 }}
               />
 
