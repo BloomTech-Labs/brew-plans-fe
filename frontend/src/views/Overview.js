33094982b6b0465d755413d54c98b060d2d88e1e
tlaudahl <travis.laudahl@gmail.com>
2020-01-10 14:29:33 -0600

    Finished the styling for the RecipeSteps and added the Overview component

---
+++ frontend/src/views/Overview.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { useKeepAwake } from 'expo-keep-awake';
+
+import { View, ScrollView, Text, Image, StyleSheet, StatusBar } from "react-native";
+
+import Layout from "../components/Layout/Layout";
+import NavBar from "../components/Layout/NavBar/NavBar.js";
+import styles from "../styling/SeededRecipesStyling";
+import Timer from "../components/timer";
+
+//Recipe
+const Overview = props => {
+    useKeepAwake();
+    const [sortedInstructions, setSortedInstructions] = useState([]);
+
+    const [timerArray, setTimerArray] = useState([]);
+
+    const { currentRecipe } = props;
+    const { instructions } = currentRecipe;
+  //captures strings starting with integers
+    const regex = new RegExp(/^\d+/g);
+
+
+    useEffect(() => {
+    if (instructions) {
+        const instructionsArray = instructions.split("////");
+        let localTimerArray = [];
+        let localInstructions = [];
+
+        instructionsArray.map((instruction, index) => {
+        let step = `Step ${index + 1}: `;
+
+        const result = instruction.match(regex);
+
+        if (result) {
+            localTimerArray.push(parseInt(result[0]));
+            instruction = instruction.substr(instruction.indexOf(" ") + 1);
+        } else {
+            localTimerArray.push(0);
+        }
+
+        let res = step.concat(instruction);
+        localInstructions.push(res);
+        });
+
+        setSortedInstructions([...localInstructions]);
+        setTimerArray([...localTimerArray]);
+    }
+    }, []);
+
+    return (
+    <View style={{ flex: 1, width: "100%" }}>
+        <Image
+        source={require("../../assets/RecipeImage.png")}
+        style={{ width: "101%", height: "20%", paddingTop: 24 }}
+        />
+        <View style={{ width: '100%', zIndex: 150 }}>
+            <Text style={{ paddingVertical: '3%', color: 'white', fontSize: 30, fontWeight: 'bold', textAlign: 'center', backgroundColor: '#231c1c' }}>{currentRecipe.title}</Text>
+        </View>
+        <Layout>
+        <ScrollView>
+            <View style={styles.recipeDetails}>
+            <Text style={styles.recipeDetailItem}>
+                Brew Type: {currentRecipe.brew_type}
+            </Text>
+            <Text style={styles.recipeDetailItem}>
+                Water Temperature: {currentRecipe.water_temp}
+            </Text>
+
+            <ScrollView>
+
+                {sortedInstructions.map((instruction, index) => (
+                <View
+                    key={index}
+                    style={{ borderBottomWidth: 0.8, borderBottomColor: "black" }}
+                >
+                <Text
+                    style={{
+                    marginVertical: 5,
+                    backgroundColor: "white",
+                    padding: 6,
+                    fontSize: 16
+                    // borderBottomWidth: 0.8,
+                    // borderBottomColor: "black"
+                    }}
+                    key={index}
+                >
+                    {instruction}
+                </Text>
+                </View>
+                ))}
+            </ScrollView>
+            </View>
+        </ScrollView>
+        </Layout>
+    </View>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+    currentRecipe: state.user.currentRecipe
+    };
+};
+
+export default connect(mapStateToProps)(Overview);
--- frontend/src/views/RecipeSteps.js
+++ frontend/src/views/RecipeSteps.js
@@ -6,8 +6,6 @@ import { useKeepAwake } from 'expo-keep-awake';
 import NavBar from "../components/Layout/NavBar/NavBar.js";
 
 
-
-
 function RecipeSteps(props) {
     useKeepAwake();
     const { currentRecipe } = props;
@@ -18,81 +16,70 @@ function RecipeSteps(props) {
     const [timerArray, setTimerArray] = useState([]);
 
 
-  const regex = new RegExp(/^\d+/g);
+    const regex = new RegExp(/^\d+/g);
 
-  
-      useEffect(() => {
+
+    useEffect(() => {
         console.log(step);
         if (instructions) {
-          const instructionsArray = instructions.split("////");
-          let localTimerArray = [];
-          let localInstructions = [];
+            const instructionsArray = instructions.split("////");
+            let localTimerArray = [];
+            let localInstructions = [];
     
-          instructionsArray.map((instruction, index) => {
+            instructionsArray.map((instruction, index) => {
             let step = '';
     
             const result = instruction.match(regex);
     
             if (result) {
-              localTimerArray.push(parseInt(result[0]));
-              instruction = instruction.substr(instruction.indexOf(" ") + 1);
+                localTimerArray.push(parseInt(result[0]));
+                instruction = instruction.substr(instruction.indexOf(" ") + 1);
             } else {
-              localTimerArray.push(0);
+                localTimerArray.push(0);
             }
     
             let res = step.concat(instruction);
             localInstructions.push(res);
-          });
+        });
     
-          setSortedInstructions([...localInstructions]);
-          setTimerArray([...localTimerArray]);
+            setSortedInstructions([...localInstructions]);
+            setTimerArray([...localTimerArray]);
         }
-      }, [step]);
+    }, [step]);
     
     
     return (
         <View style={ styles.mainView }>
             <NavBar {...props} />
-            <Image
-            source={require("../../assets/RecipeImage.png")}
-            style={{ width: "100%", height: "20%" }}
-            />
             <Text style={ styles.recipeTitle }>{currentRecipe.title}</Text>
-           
-       <View style={ styles.recipeInfo }> 
-            {/* <TouchableOpacity onPress={() => props.navigation.navigate('StartBrew')}> */}
-
-            {/* <TouchableOpacity {...step === 1 ? onPress=() => {props.navigation.navigate('StartBrew')} : onPress=() => {step-1}} > */}
-            <TouchableOpacity onPress={() => {step === 0 ? props.navigation.navigate('StartBrew') : setStep(step-1)}}>
-
-
-             <Image
-                source={require('../../assets/previous.png')}
-                style={{ marginVertical: 10 }}
-                /> 
-                </TouchableOpacity>
-                <Text style={{color: 'white', fontSize:30, fontWeight: 'bold' }}>Step {step+1} </Text> 
-            <TouchableOpacity onPress={() => setStep(step+1)}>
-
-                <Image
-                source={require('../../assets/next.png')}
-                style={{ marginVertical: 10 }}
-                /> 
-                </TouchableOpacity>
-             
-                
-            </View>
-            <View style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+            <View style={{ justifyContent: 'space-around', alignItems: 'center', height: '33%' }}>
                 <Image
                 source={require('../../assets/kettle-outline1.png')}
                 style={{ marginVertical: 10 }}
                 />
-             <Text style={{fontSize:18}}>{sortedInstructions[step]}</Text>   
-             {/* <Text style={{fontSize:18}}> Bring at least 600 grams(20oz) of water to a boil.</Text>    */}
+            <Text style={{fontSize:18}}>{sortedInstructions[step]}</Text>
             </View>
-            <View style={ styles.overview }>
-                <Text style={ styles.overviewText }>Overview</Text>
-                <Text style={ styles.overviewText }>+</Text>
+            <View style={ styles.bottomContainer }>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#1f2233' }}>
+                    <TouchableOpacity onPress={() => {step === 0 ? props.navigation.navigate('StartBrew') : setStep(step-1)}}>
+                    <Image
+                        source={require('../../assets/previous.png')}
+                        style={{ marginVertical: 10 }}
+                        /> 
+                        </TouchableOpacity>
+                        <Text style={{color: 'white', fontSize:30, fontWeight: 'bold' }}>Step {step+1} </Text> 
+                    <TouchableOpacity onPress={() => setStep(step+1)}>
+
+                        <Image
+                        source={require('../../assets/next.png')}
+                        style={{ marginVertical: 10 }}
+                        /> 
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#1f2233' }}>
+                    <Text style={ styles.overviewText }>Overview</Text>
+                    <Text style={ styles.overviewText }>+</Text>
+                </View>
             </View>
         </View>
     )
@@ -102,33 +89,21 @@ const styles = StyleSheet.create({
     overviewText: {
         fontSize: 30,
         fontWeight: 'bold',
-        color: 'white'
+        color: 'white',
+        padding: '2%'
     },
-    overview: {
-        backgroundColor: '#231c1c', 
+    bottomContainer: { 
         width: '100%', 
         position: 'absolute', 
         bottom: 0, 
-        justifyContent: 'space-between', 
-        flexDirection: 'row', 
-        padding: '2%'
-    },
-    recipeInfoText: {
-        color: 'white',
-        fontSize: 20
-    },
-    recipeInfo: {
-        width: '100%', 
-        height: '12%', 
-        backgroundColor: '#231c1c', 
         justifyContent: 'space-between',
-        alignItems: 'center',
-        flexDirection: "row"
+        height: '20%'
     },
     recipeTitle: {
         fontSize: 30, 
         alignSelf: 'center', 
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginTop: 30
     },
     mainView: {
         width: '100%',
