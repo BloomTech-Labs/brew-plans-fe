41c9dc6f0b89682c75c05f147dad4729a8bdbbf7
tlaudahl <travis.laudahl@gmail.com>
2020-01-14 18:39:25 -0600

    Got the ADD button showing next to Recipe View, still need to get the AddNewRecipe Modal to open

--- frontend/src/styling/MyRecipesStyling.js
+++ frontend/src/styling/MyRecipesStyling.js
@@ -2,10 +2,7 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
     pageContainer: {
-      flex: .85,
-      justifyContent: 'flex-start',
-      padding: 20,
-      backgroundColor: 'white',
+      padding: 20
     },
     recipesHeader: {
       flexDirection: 'row',
@@ -21,21 +18,15 @@ export default StyleSheet.create({
       alignSelf: 'center'
     },
     navbar: {
-      width:'100%',
-      height: 80,
-      flexDirection: 'column',
+      width:'96%',
+      flexDirection: 'row',
       justifyContent: 'space-between',
-    },
-    navbarButton: {
-      width: '96%',
-      alignSelf: 'center',
+      alignItems: 'center',
       backgroundColor: '#1F2233',
       borderRadius: 2,
-      justifyContent: 'center',
-      marginTop: 15
-    },  
+      marginVertical: 10
+    }, 
     navbarText: {
-      textAlign: 'left',
       padding: 13,
       color: 'white',
       fontSize: 27
