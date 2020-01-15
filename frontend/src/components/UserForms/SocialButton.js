7963b60509f775faf0c549f00a7ac9e2b0b5993b
Jasmine <jasminecharles@hotmail.com>
2020-01-15 15:58:44 -0800

    header and inputs alignment

--- frontend/src/components/UserForms/SocialButton.js
+++ frontend/src/components/UserForms/SocialButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { IconButton } from 'react-native-paper';
-import { TouchableOpacity, Image } from 'react-native';
+import { TouchableOpacity, Image, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { connect } from 'react-redux';
 import { googleSignIn } from '../../store/actions/index.js';
@@ -8,12 +8,17 @@ import { googleSignIn } from '../../store/actions/index.js';
 const SocialButton = props => {
   // console.log(props);
   return (
+    <View>
     <TouchableOpacity onPress={() => props.googleSignIn(props.loginConfig)}>
     <Image style={{width: 280, height: 42, borderWidth: 2, borderColor:'#1F2233', marginLeft: -96, marginTop: 60, marginBottom: 22}} source={require('../../../assets/google.png')}/>
-    <Image style={{width: 280, height: 42, borderWidth: 2, borderColor:'#1F2233', marginLeft: -96}} source={require('../../../assets/facebook.png')}/>
-
+    
       {/* <Ionicons name={props.icon} size={36} color={'black'} /> */}
     </TouchableOpacity>
+    <TouchableOpacity>
+    <Image style={{width: 280, height: 42, borderWidth: 2, borderColor:'#1F2233', marginLeft: -96}} source={require('../../../assets/facebook.png')}/>
+    
+    </TouchableOpacity>
+    </View>
   );
 };
 
