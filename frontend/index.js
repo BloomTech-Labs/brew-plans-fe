3caf1d80ded60ab3ea12a1fda0e98a2c7b1c29dc
Sydney Blom <sydneyblom@#gmail.com>
2020-01-24 12:23:46 -0600

    needed to save index file

--- frontend/index.js
+++ frontend/index.js
@@ -4,18 +4,16 @@ import App from './App.js';
 import configureStore from './src/store/configureStore.js';
 import { registerRootComponent } from 'expo';
 import { StatusBar, View } from 'react-native';
-// import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 const store = configureStore();
 
 const RNRedux = () => (
- // <SafeAreaProvider>
+
   <Provider store={store}>
     <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
       <App />
     </View>
   </Provider>
- // </SafeAreaProvider>
+
 )
 
 // Register the app container
