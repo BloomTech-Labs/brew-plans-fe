529189e7298e5dda452ff63ded4ad72faa942650
Jasmine <jasminecharles@hotmail.com>
2020-01-24 13:42:02 -0800

    facebook login now working

--- frontend/src/components/UserForms/FacebookLogin.js
+++ frontend/src/components/UserForms/FacebookLogin.js
@@ -19,29 +19,6 @@ class FacebookLogin extends React.Component {
         })
     }
 
-    // async function logIn() {
-    //     try {
-    //       await Facebook.initializeAsync('<APP_ID>');
-    //       const {
-    //         type,
-    //         token,
-    //         expires,
-    //         permissions,
-    //         declinedPermissions,
-    //       } = await Facebook.logInWithReadPermissionsAsync({
-    //         permissions: ['public_profile'],
-    //       });
-    //       if (type === 'success') {
-    //         // Get the user's name using Facebook's Graph API
-    //         const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-    //         Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
-    //       } else {
-    //         // type === 'cancel'
-    //       }
-    //     } catch ({ message }) {
-    //       alert(`Facebook Login Error: ${message}`);
-    //     }
-    //   }
 
       
     async loginWithFacebook() {
